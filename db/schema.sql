35e690976bf66518c243a231f7f5e25e5b758e98
devramirez <aramirezdev1@gmail.com>
2023-07-21 17:20:05 -0400

    created table role in schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -1,6 +1,15 @@
 -- create department table
 CREATE TABLE department (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(30)
+    name VARCHAR(30),
 );
+
+-- create table role
+CREATE TABLE role (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30),
+    salary DECIMAL,
+    department_id INTEGER,
+);
+
 
