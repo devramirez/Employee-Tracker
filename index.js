7b676a34c38aa2f54b4ac771c1693e38a9662d47
devramirez <aramirezdev1@gmail.com>
2023-07-21 18:43:24 -0400

    created employee_tracker func for inquirer

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-// importing modules
+// importing modules/dependencies required
 const express = require("express");
 const mysql = require("mysql");
-const sequelize = require("sequelize")
+// const sequelize = require("sequelize")
+const db = require("./db/connection");
+const { default: inquirer } = require("inquirer");
 
 // create bridge between nodejs & mysql
 // create connection
@@ -9,7 +11,7 @@ const db = mysql.createConnection({
     host: "localhost",
     user: "root",
     password: "mySQL0629$%",
-    database: "nodemysql",
+    database: "employee_tracker_db",
 });
 
 // connect to MySQL
@@ -18,4 +20,25 @@ db.connect((err) => {
         throw err;
     }
     console.log("MySQL Connected")
+    employee_tracker();
 });
+
+const employee_tracker = function() {
+    inquirer.prompt([{
+        // starting prompt
+        type: 'list',
+        name: 'prompt',
+        message: 'What would you like to do?',
+        choices: ['View All Departments', 'View All Roles', 'View All Employees', 'Add a Department', 'Add a Role', 'Add an Employee', 'Update an Employee Role', 'Exit']
+    }]).then((answers) => {
+        // points to department table in db
+        if (answers.prompt === 'View All Departments') {
+            db.query(`SELECT * FROM department`, (err, result) => {
+                if (err) throw err;
+                console.log('Viewing All Departments: ');
+                console.table(result);
+                employee_tracker();
+            })
+        }
+    })
+}
