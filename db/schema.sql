23d21e718bc66a9582f76ed644fb068d8e73eb1c
devramirez <aramirezdev1@gmail.com>
2023-07-21 17:21:30 -0400

    created employee table in schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -12,4 +12,11 @@ CREATE TABLE role (
     department_id INTEGER,
 );
 
-
+-- create employee table
+CREATE TABLE employee (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INTEGER,
+    manager_id INTEGER,
+);
