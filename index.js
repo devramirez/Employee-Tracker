9a5926b7868d846258324fd528426591a46ba396
devramirez <aramirezdev1@gmail.com>
2023-08-24 01:06:30 -0400

    added functions to view roles & show employees

--- index.js
+++ index.js
@@ -82,20 +82,91 @@ const startApp = () => {
   };
 
   // Function to view all roles within database
-const viewAllRoles = () => {
-  // The single letters represent the first letter of each table within seeds.sql. This is then connected to the corresponding label issued in the schema.sql file to display that selected data.
-  // This pulls from the employee table and joins the role table and department table data to it
-    connection.query(`SELECT 
+  const viewAllRoles = () => {
+    // This pulls from the employee table and joins the role table and department table data to it
+    connection.query(
+      `SELECT 
      r.id,
      r.title,
      d.name AS department,
      r.salary
      FROM role r
-     JOIN department d ON r.department_id = d.id`, 
-     (err, res) => {
+     JOIN department d ON r.department_id = d.id`,
+      (err, res) => {
         if (err) throw err;
         console.table(res);
         startApp();
+      }
+    );
+  };
+
+// Function to show all employees
+const viewAllEmployees = () => {
+  // Similar to viewAllRoles, this function uses SQL commands to display select data and joins tables together into a cohesive table display
+  connection.query(
+    `
+    SELECT 
+      e.id, 
+      e.first_name, 
+      e.last_name, 
+      r.title, 
+      d.name AS department, 
+      r.salary, 
+      CONCAT(m.first_name, ' ', m.last_name) AS manager
+    FROM employee e
+    JOIN role r ON e.role_id = r.id
+    JOIN department d ON r.department_id = d.id
+    LEFT JOIN employee m ON e.manager_id = m.id
+  `,
+    (err, res) => {
+      if (err) throw err;
+      console.table(res);
+      startApp();
+    }
+  );
+};
+
+// Function to add a role
+const addRole = () => {
+    connection.query('SELECT id, name FROM department', (err, res) => {
+        if (err) throw err;
+
+        const departmentNames = res.reduce((acc, curr) => {
+            acc[curr.name] = curr.id;
+            return acc;
+        }, {});
+    
+   inquirer.prompt([
+    {
+        name: 'role',
+        type: 'input',
+        message: 'Please enter your role in the company.'
+    },
+    {
+        name: 'salary',
+        type: 'input',
+        message: 'Please enter your salary.'
+    },
+    {
+        name: 'department',
+        type: 'list',
+        message: 'Please select which department your role is in.',
+        // Pulls from object to display department names in seeds.sql table
+        choices: Object.keys(departmentNames),
+    }
+   ]).then(answer => {
+    const departmentID = departmentNames[answer.department];
+        connection.query(
+                'INSERT INTO role (title, salary, department_id) VALUES (?, ?, ?)',
+                [answer.role, answer.salary, departmentID],
+                function (err, res) {
+                    if (err) throw err;
+                    console.log('Role added!');
+                    startApp();
+                }
+            );
+        })
     });
 };
+
 };
